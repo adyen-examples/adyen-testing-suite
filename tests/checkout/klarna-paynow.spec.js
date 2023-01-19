8424d0deb66db4f63e646758fdc3c4718991e88c
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-01-19 21:18:39 +0100

    Do not redetected iframe every line. Use existing frame. (#15)
    Comments are uniform

--- tests/checkout/klarna-paynow.spec.js
+++ tests/checkout/klarna-paynow.spec.js
@@ -2,22 +2,20 @@
 const { test, expect } = require('@playwright/test');
 
 test('Klarna Pay Now', async ({ page }) => {
-
     await page.goto('/');
 
     await expect(page).toHaveTitle(/Checkout Demo/);
     await expect(page.locator('text="Select a demo"')).toBeVisible();
 
-    // select Klarna Pay now
+    // Select "Klarna Pay Now"
     await page.locator('text="Klarna - Pay now"').click();
     await expect(page.locator('text="Cart"')).toBeVisible();
 
-    // click "Continue to checkout"
+    // Click "Continue to checkout"
     await page.click('text="Continue to checkout"');
     await expect(page.locator('text="Continue to Pay now with Klarna."')).toBeVisible();
 
-    // click "Continue to Klarna"
+    // Click "Continue to Klarna"
     await page.locator('text="Continue to Pay now with Klarna."').click();
     await expect(page).toHaveTitle("Je bestelling afronden");
-
 });
