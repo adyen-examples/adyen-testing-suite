c8d009f5d916477382806305e0b5bd15ecfd2a8d
gcatanese <gcatanese@yahoo.com>
2023-03-27 15:47:50 +0200

    Test payment with invalid token

---
+++ tests/subscription/make-payment-failure.spec.js
@@ -0,0 +1,14 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Make Payment (failure)', async ({ page }) => {
+    // call to invalid token
+    await page.goto('/admin/makepayment/12345');
+
+    // check error message
+    await expect(page.locator('text="Payment failed for RecurringDetailReference"')).toBeVisible();
+    // check error icon
+    await expect(page.locator('.status-image >> visible=true')).toBeVisible();
+
+    await expect(page.locator('text="Return"')).toBeVisible();
+});
