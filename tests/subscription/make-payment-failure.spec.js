9bd9ff9ab2e7d3b4c93e3d45019230aca881323a
gcatanese <gcatanese@yahoo.com>
2023-03-28 12:45:56 +0200

    Use case-insensitive partial match

--- tests/subscription/make-payment-failure.spec.js
+++ tests/subscription/make-payment-failure.spec.js
@@ -6,7 +6,7 @@ test('Make Payment (failure)', async ({ page }) => {
     await page.goto('/admin/makepayment/12345');
 
     // check error message
-    await expect(page.locator('text="Payment failed for RecurringDetailReference"')).toBeVisible();
+    await expect(page.locator('text=Payment failed for RecurringDetailReference')).toBeVisible();
     // check error icon
     await expect(page.locator('.status-image >> visible=true')).toBeVisible();
 
