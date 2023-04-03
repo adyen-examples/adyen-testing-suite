e464d0e389752071679cc784b1614a40e1e8c6fa
Kwok He Chu <>
2023-04-03 09:05:28 +0200

    E2E giftcard givex/generic giftcard

---
+++ tests/giftcard/component-givex-giftcard.js
@@ -0,0 +1,64 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Dropin Givex', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page).toHaveTitle(/Giftcard Demo/);
+    await expect(page.locator('text="Select a demo"')).toBeVisible();
+
+    // Select "Drop-in"
+    await page.getByRole('link', { name: 'Giftcard' }).click();
+    await expect(page.locator('text="Cart"')).toBeVisible();
+
+    // Click "Continue to checkout"
+    await page.getByRole('link', { name: 'Continue to checkout' }).click();
+    
+    // Wait for network state to be idle
+    await page.waitForLoadState('networkidle');
+
+    // Enter giftcard #1 // 110 EUR
+    await enterGiftcardDetails(page);
+
+    // Enter giftcard #2 // 60 EUR
+    await enterGiftcardDetails(page);
+
+    // Enter giftcard #3 // 10 EUR (final payment)
+    await enterGiftcardDetails(page);
+    
+    // Click "Pay" button
+    const payButton = page.locator('.adyen-checkout__button__text >> visible=true');
+    await expect(payButton).toBeVisible();
+    await payButton.click();
+
+    await expect(page.locator('text="Return Home"')).toBeVisible();
+});
+
+async function enterGiftcardDetails(page) {
+    // Click "Credit or debit card"
+    const radioButton = await page.getByRole('radio', { name: 'Givex' });
+    if (await radioButton.count() === 0) {
+        // Click normal button for < Adyen-Web 5.32.x or lower
+        await page.getByRole('button', { name: 'Givex' }).click();
+    }
+    else {
+        // Click radio button for > Adyen-Web 5.33.x or higher
+        await radioButton.click();
+    }
+    
+    // Find iframe and fill "Card number" field
+    const cardNumberFrame = page.frameLocator('internal:attr=[title="Iframe for secured gift card number"i]');
+    await cardNumberFrame.getByPlaceholder('1234 5678 9012 3456').fill('6036280000000000000');
+
+    // Find iframe and fill "Pin" field
+    const expiryDateFrame = page.frameLocator('internal:attr=[title="Iframe for secured gift card security code"i]');
+    await expiryDateFrame.getByPlaceholder('123').fill('123');
+    
+    // Click "Redeem" button
+    const redeemButton = page.locator('.adyen-checkout__button__text >> visible=true');
+    await expect(redeemButton).toBeVisible();
+    await redeemButton.click();
+    
+    // Wait for network state to be idle
+    await page.waitForLoadState('networkidle');
+}
