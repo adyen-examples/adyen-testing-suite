1da7c8e8520fbf6f7c160254f61cb3e213e3a019
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-05-16 12:59:44 +0200

    E2E testing until the 3rd-party page

--- tests/paybylink/paybylink.spec.js
+++ tests/paybylink/paybylink.spec.js
@@ -21,7 +21,7 @@ test('Pay By Link', async ({ page }) => {
     // Wait for network state to be idle
     await page.waitForLoadState('networkidle');
 
-    // Ensure the link with `uniqueReference ` is created
+    // Ensure the link with `uniqueReference` is created
     await expect(page.getByText(uniqueReference)).toBeVisible();
 
     const link = await page.locator('text=/PL/').first();
@@ -30,42 +30,6 @@ test('Pay By Link', async ({ page }) => {
     // Wait for network state to be idle
     await page.waitForLoadState('networkidle');
 
-    // Click 'Accept' (terms and conditions) button
-    await page.getByRole('button', { name: 'Accept' }).click();
-
-    // Enter card scheme details
-    await enterSchemeDetails(page);
-
-    // Click 'Pay' button
-    const payButton = page.locator('.adyen-checkout__button__text >> visible=true');
-    await expect(payButton).toBeVisible();
-    await payButton.click();
-
-    // Wait for network state to be idle
-    await page.waitForLoadState('networkidle');
-
-    // Payment success
-    await page.getByText('Payment successful!');
+    // Check Pay by link text
+    await page.getByText(/Pay by link/);
 });
-
-async function enterSchemeDetails(page) {
-    // Click "Credit Card"
-    await page.getByRole('radio', { name: 'Credit Card' }).click();
-
-    // Wait for network state to be idle
-    await page.waitForLoadState('networkidle')
-
-    // Find iframe and fill "Card number" field
-    const cardNumberFrame = await page.frameLocator('iframe[title*="card number"]');
-    await cardNumberFrame.getByPlaceholder('1234 5678 9012 3456').fill('4166 6766 6766 6746');
-
-    // Find iframe and fill "Expiry date" field
-    const expiryDateFrame = await page.frameLocator('iframe[title*="expiry date"]');
-    await expiryDateFrame.getByPlaceholder('MM/YY').fill('03/30');
-
-    // Find iframe for CVC
-    const cvcFrame = await page.getByRole('region[name="Credit Card"i]').frameLocator('iframe[title*="security code"]');
-
-    // Fill "CVC / CVV" field
-    await cvcFrame.getByPlaceholder('3 digits').fill('737');
-}
