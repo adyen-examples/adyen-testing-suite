b4291c1cb9a4290556c490cd0804a101ce919067
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2023-06-07 16:53:46 +0200

    Added card, dropin-card and ideal E2E tests to checkout-advanced-example

---
+++ tests/checkout-advanced/dropin-card.spec.js
@@ -0,0 +1,60 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Dropin Card', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page).toHaveTitle(/Checkout Demo Advanced/);
+    await expect(page.locator('text="Select a demo"')).toBeVisible();
+
+    // Select "Drop-in"
+    await page.getByRole('link', { name: 'Drop-in' }).click();
+    await expect(page.locator('text="Cart"')).toBeVisible();
+
+    // Click "Continue to checkout"
+    await page.getByRole('link', { name: 'Continue to checkout' }).click();
+
+    // Wait for network state to be idle
+    await page.waitForLoadState('networkidle');
+    
+    // Assert that "Credit or debit card" is visible
+    await expect(page.locator('text="Credit or debit card"')).toBeVisible();
+
+    // Click "Credit or debit card"
+    const radioButton = await page.getByRole('radio', { name: 'Credit or debit card' });
+    if (await radioButton.count() === 0) {
+        // Click normal button for < Adyen-Web 5.32.x or lower
+        await page.getByRole('button', { name: 'Credit or debit card' }).click();
+    }
+    else {
+        // Click radio button for > Adyen-Web 5.33.x or higher
+        await radioButton.click();
+    }
+
+    // Wait for network state to be idle
+    await page.waitForLoadState('networkidle')
+    
+    // Find iframe and fill "Card number" field
+    const cardNumberFrame = await page.frameLocator('iframe[title*="card number"]');
+    await cardNumberFrame.getByPlaceholder('1234 5678 9012 3456').fill('4166 6766 6766 6746');
+
+    // Find iframe and fill "Expiry date" field
+    const expiryDateFrame = await page.frameLocator('iframe[title*="expiry date"]');
+    await expiryDateFrame.getByPlaceholder('MM/YY').fill('03/30');
+
+    // Find iframe for CVC
+    const cvcFrame = await page.getByRole('region[name="Credit or debit card"i]').frameLocator('iframe[title*="security code"]');
+    
+    // Fill "CVC / CVV" field
+    await cvcFrame.getByPlaceholder('3 digits').fill('737');
+   
+    // Find and fill "Name on card" field - Note: this field is not contained within an iframe
+    await page.getByPlaceholder('J. Smith').fill('J. Smith');
+
+    // Click "Pay" button
+    const payButton = page.locator('.adyen-checkout__button__text >> visible=true');
+    await expect(payButton).toBeVisible();
+    await payButton.click();
+    
+    await expect(page.locator('text="Return Home"')).toBeVisible();
+});
---
+++ tests/checkout-advanced/webhook.spec.js
@@ -0,0 +1,41 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Webhook Notification', async ({ request }) => {
+    const notifications = await request.post(`/api/webhooks/notifications`, {
+        data: {
+            "live": "false",
+            "notificationItems":[
+                {
+                    "NotificationRequestItem":{
+                        "additionalData":{
+                            "hmacSignature":"+JWKfq4ynALK+FFzGgHnp1jSMQJMBJeb87dlph24sXw="
+                        },
+                        "eventCode":"AUTHORISATION",
+                        "success":"true",
+                        "eventDate":"2019-06-28T18:03:50+01:00",
+                        "merchantAccountCode":"YOUR_MERCHANT_ACCOUNT",
+                        "pspReference": "7914073381342284",
+                        "merchantReference": "YOUR_REFERENCE",
+                        "amount": {
+                            "value":1130,
+                            "currency":"EUR"
+                        }
+                    }
+                }
+            ]
+        }
+    });
+
+    /// Verify notification is not accepted (invalid HMAC)
+
+    // Status code not 404 (verify webhook is found)
+    expect(notifications.status()).not.toEqual(404);
+
+    // Status code not 200 (verify webhook does not accept the notification ie HMAC invalid)
+    expect(notifications.status()).not.toEqual(200);
+
+    // Body response does not contain [accepted]
+    notifications.text()
+        .then(value => {expect(value).not.toEqual("[accepted]");} );
+});
