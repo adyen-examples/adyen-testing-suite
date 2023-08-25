a107d089e85557b84acd81c85caf9093bd83f3a8
Julien Lengrand-Lambert <julien@lengrand.fr>
2023-08-25 14:41:09 +0200

    Move tests to separate files

--- tests/giving/card-and-donation-cancel.spec.js
+++ tests/giving/card-and-donation-cancel.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('Card and donate', async ({ page }) => {
+test('Card and Cancel', async ({ page }) => {
     await page.goto('/');
 
     await expect(page).toHaveTitle(/Checkout Demo - Select type/);
@@ -15,10 +15,10 @@ test('Card and donate', async ({ page }) => {
 
     // Wait for network state to be idle
     await page.waitForLoadState('networkidle');
-    
+
     // Assert that "Card number" is visible within iframe
     await expect(page.locator('text="Card number"')).toBeVisible();
-    
+
     // Find iframe and fill "Card number" field
     const cardNumberFrame = await page.frameLocator('iframe[title*="card number"]');
     await cardNumberFrame.getByPlaceholder('1234 5678 9012 3456').fill('5555 3412 4444 1115');
@@ -30,7 +30,7 @@ test('Card and donate', async ({ page }) => {
     // Find iframe and fill "CVC / CVV" field
     const cvcFrame = await page.frameLocator('iframe[title*="security code"]');
     await cvcFrame.getByPlaceholder('3 digits').fill('737');
-   
+
     // Find and fill "Name on card" field - Note: this field is not contained within an iframe
     await page.getByPlaceholder('J. Smith').fill('J. Smith');
 
@@ -40,8 +40,7 @@ test('Card and donate', async ({ page }) => {
     await payButton.click();
 
     // Click Amount button
-    await page.getByText('3.00').click();
-    await page.getByText('Donate').click();
+    await page.getByText('Not now').click();
 
-    await expect(page.locator('text="Thank you for your donation!"')).toBeVisible();
+    await expect(page.locator('text="Donate"')).toBeHidden();
 });
