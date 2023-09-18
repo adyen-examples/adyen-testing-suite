3197ce22200de593c30fa1c7368e89486fe26e06
Beppe Catanese <gcatanese@yahoo.com>
2023-09-18 16:22:35 +0200

    Add webhook tests

---
+++ tests/giving/webhook.spec.js
@@ -0,0 +1,45 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const utilities = require('../utilities');
+
+// test webhook is successfully delivered
+test('Webhook Notification', async ({ request }) => {
+
+    var notificationRequestItem = {
+        "eventCode":"AUTHORISATION",
+        "success":"true",
+        "eventDate":"2019-06-28T18:03:50+01:00",
+        "merchantAccountCode":"YOUR_MERCHANT_ACCOUNT",
+        "pspReference": "7914073381342284",
+        "merchantReference": "YOUR_REFERENCE",
+        "amount": {
+            "value":1130,
+            "currency":"EUR"
+        }
+    };
+
+    // calculate signature from payload
+    const hmacSignature = await utilities.calculateHmacSignature(notificationRequestItem);
+    // add hmacSignature to 'additionalData'
+    notificationRequestItem["additionalData"] = {"hmacSignature" : ""+hmacSignature+""}
+
+    // POST webhook
+    const notifications = await request.post(`/api/webhooks/notifications`, {
+        data: {
+            "live": "false",
+            "notificationItems":[
+                {
+                    "NotificationRequestItem": notificationRequestItem    
+                }           
+            ]
+        }
+    });
+
+    // Verify status code 
+    expect(notifications.status()).toEqual(200);
+
+    // Verify body response 
+    notifications.text()
+        .then(value => {expect(value).toEqual("[accepted]");} );
+});
+
