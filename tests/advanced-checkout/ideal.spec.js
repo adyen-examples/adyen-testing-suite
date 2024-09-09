63adf4bf145c4f5e44cf23472be4f682b64a49db
Beppe Catanese <gcatanese@yahoo.com>
2024-09-09 22:55:48 +0200

    Support iDEAL and iDEAL 2 flows

--- tests/advanced-checkout/ideal.spec.js
+++ tests/advanced-checkout/ideal.spec.js
@@ -10,18 +10,30 @@ test('iDEAL', async ({ page }) => {
     // Select "iDEAL"
     await page.getByRole('link', { name: 'iDEAL' }).click();
     await expect(page.locator('text="Cart"')).toBeVisible();
-
+    
     // Click "Continue to checkout"
     await page.getByRole('link', { name: 'Continue to checkout' }).click();
 
-    // Click "Select your bank"
-    const selectButton = await page.getByTitle('Select your bank');
-    if (await selectButton.count() === 0) {
-        // Click radio button for > Adyen-Web 5.53.x or higher
-        await page.getByPlaceholder('Search...').click();
+    // Check if "Continue to iDEAL" button is found
+    const ideal2Button = await page.locator('Continue to iDEAL').isVisible();
+
+    if (ideal2Button) {
+        // run iDEAL2 flow
+        console.log("Running iDEAL 2.0 flow");
+        await ideal2(page);
     } else {
-        await selectButton.click();
+        // run iDEAL 1 flow
+        console.log("Running legacy iDEAL flow");
+        await ideal1(page);
     }
+
+});
+
+// run through the legacy iDEAL flow (including bank selection)
+async function ideal1(page) {
+
+    // Click "Select your bank"
+    await page.getByPlaceholder('Search...').click();
 
     // Choose "Test Issuer"
     await page.getByText('Test Issuer', { exact: true }).click();
@@ -33,4 +45,25 @@ test('iDEAL', async ({ page }) => {
     // Click "Continue"  
     await page.getByRole('button', { name: 'Continue' }).click();
     await page.getByRole('link', { name: 'Return Home' }).click();
-});
+}
+
+// run through the iDEAL 2.0 flow (redirect to iDEAL page)
+async function ideal2(page) {
+
+    // Click "Continue to iDEAL"
+    await page.getByRole('button', { name: 'Continue to iDEAL' }).click();
+    await expect(page.locator('text="Scan with your banking app to pay"')).toBeVisible();
+
+    // Click "Select your bank"
+    await page.getByRole('button', { name: 'Select your bank' }).click();
+
+    // Click "TESTNL2A"
+    await page.getByRole('button', { name: 'TESTNL2A' }).click();
+    await expect(page.locator('text="Which test simulation to run?"')).toBeVisible();
+
+    // Click "Success"
+    await page.getByRole('button', { name: 'Success' }).click();
+
+    // Click "Continue"  
+    await page.getByRole('link', { name: 'Return Home' }).click();
+}
--- tests/checkout/ideal.spec.js
+++ tests/checkout/ideal.spec.js
@@ -14,6 +14,24 @@ test('iDEAL', async ({ page }) => {
     // Click "Continue to checkout"
     await page.getByRole('link', { name: 'Continue to checkout' }).click();
 
+    // Check if "Continue to iDEAL" button is found
+    const ideal2Button = await page.locator('Continue to iDEAL').isVisible();
+
+    if (ideal2Button) {
+        // run iDEAL2 flow
+        console.log("Running iDEAL 2.0 flow");
+        await ideal2(page);
+    } else {
+        // run iDEAL 1 flow
+        console.log("Running legacy iDEAL flow");
+        await ideal1(page);
+    }
+
+});
+
+// run through the legacy iDEAL flow (including bank selection)
+async function ideal1(page) {
+
     // Click "Select your bank"
     await page.getByPlaceholder('Search...').click();
 
@@ -27,4 +45,25 @@ test('iDEAL', async ({ page }) => {
     // Click "Continue"  
     await page.getByRole('button', { name: 'Continue' }).click();
     await page.getByRole('link', { name: 'Return Home' }).click();
-});
+}
+
+// run through the iDEAL 2.0 flow (redirect to iDEAL page)
+async function ideal2(page) {
+
+    // Click "Continue to iDEAL"
+    await page.getByRole('button', { name: 'Continue to iDEAL' }).click();
+    await expect(page.locator('text="Scan with your banking app to pay"')).toBeVisible();
+
+    // Click "Select your bank"
+    await page.getByRole('button', { name: 'Select your bank' }).click();
+
+    // Click "TESTNL2A"
+    await page.getByRole('button', { name: 'TESTNL2A' }).click();
+    await expect(page.locator('text="Which test simulation to run?"')).toBeVisible();
+
+    // Click "Success"
+    await page.getByRole('button', { name: 'Success' }).click();
+
+    // Click "Continue"  
+    await page.getByRole('link', { name: 'Return Home' }).click();
+}
