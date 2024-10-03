0a9a3b993a4ab264161bc5e953da7abd830eb563
kwokhe <kwokhe.chu@adyen.com>
2024-10-03 11:28:39 +0200

    Add E2E test workshop

---
+++ tests/workshop/workshop-advanced-flow.spec.js
@@ -0,0 +1,37 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const utilities = require('../utilities');
+
+test('Workshop - Advanced Flow Payment', async ({ page }) => {
+    await page.goto('/');
+
+    // Select "Drop-in"
+    await expect(page.locator('text="Start shopping"')).toBeVisible();
+    await page.getByRole('link', { name: 'Start shopping' }).click();
+
+    // Click "Continue to checkout"
+    await page.getByRole('link', { name: 'Continue to checkout' }).click();
+
+    // Wait for load event
+    await page.waitForLoadState('load');
+
+    // Assert that "Credit or debit card" is visible
+    await expect(page.locator('text="Credit or debit card"')).toBeVisible();
+
+    // Click "Credit or debit card"
+    const radioButton = await page.getByRole('radio', { name: 'Credit or debit card' });
+    await radioButton.click();
+
+    // Wait for load event
+    await page.waitForLoadState('load');
+
+    // Fill card details
+    await utilities.fillDropinCardDetails(page);
+
+    // Click "Pay" button
+    const payButton = page.locator('.adyen-checkout__button__text >> visible=true');
+    await expect(payButton).toBeVisible();
+    await payButton.click();
+
+    await expect(page.locator('text="Return Home"')).toBeVisible();
+});
