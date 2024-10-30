67026be9598077a083d5ede1bb8d72f8024b6aed
Beppe Catanese <1771700+gcatanese@users.noreply.github.com>
2024-10-30 09:03:31 +0100

    Add AfP tests for Payouts and Reports pages (#99)

--- tests/afp/dashboard.spec.js
+++ tests/afp/dashboard.spec.js
@@ -37,3 +37,37 @@ test('Transactions', async ({ page }) => {
   await expect(page.locator('text="My Transactions"')).toBeVisible();
 });
 
+test('Payouts', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/AfP MyPlatform/);
+
+  await page.goto('/login');
+  await expect(page.locator('text="Login"')).toBeVisible();
+  // perform login
+  await page.fill('input[name="username"]', utilities.USERNAME);
+  await page.fill('input[name="password"]', "123");
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  await expect(page.locator('text="Dashboard"')).toBeVisible();
+
+  await page.goto('/payouts');
+  await expect(page.locator('text="My Payouts"')).toBeVisible();
+});
+
+test('Reports', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/AfP MyPlatform/);
+
+  await page.goto('/login');
+  await expect(page.locator('text="Login"')).toBeVisible();
+  // perform login
+  await page.fill('input[name="username"]', utilities.USERNAME);
+  await page.fill('input[name="password"]', "123");
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  await expect(page.locator('text="Dashboard"')).toBeVisible();
+
+  await page.goto('/reports');
+  await expect(page.locator('text="My Reports"')).toBeVisible();
+});
+
